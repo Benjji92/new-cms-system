fe27fe8df6f11a973bfc8571936f725bed273770
Benjji92 <bazsobeni@gmail.com>
2022-09-04 18:54:33 +0200

    minor changes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function show(){
+    public function show(Post $post){
 
-        return view('blog-post');
+        return view('blog-post', ['post'=>$post]);
 
     }
+
+    public function create(){
+
+        return view('admin.posts.create');
+
+    }
+
+    public function store(){
+
+        
+       $inputs = request()->validate([
+        'title'=>'required|min:8|max:255',
+        'post_image'=>'file',      //'mimes:jpeg,png'
+        'body'=>'required'
+       ]);
+
+       if (request('post-image')) {
+        $inputs['post-image'] = request('post-image')->store('images');
+       }
+
+       auth()->user()->posts()->create($inputs);
+
+       return back();
+
+      // dd($request->post_image);
+
+    }
+
 }
+
--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -14,7 +14,7 @@ class PostSeeder extends Seeder
     public function run()
     {
         for ($i = 1; $i <= \App\Models\User::all('id')->max()->id; $i++) {
-            \App\Models\Post::factory(rand(1,5))->create(["user_id"=>$i]);
+            \App\Models\Post::factory(1)->create(["user_id"=>$i]);
           }
         
     }
